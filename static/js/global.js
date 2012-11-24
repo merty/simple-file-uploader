c319a7b4452d0ca172f487615787cf3a47c4b2d0
Mert Yazicioglu <mert@mertyazicioglu.com>
2012-11-24 19:36:19 +0200

    The JS code run in the browser is improved.

--- static/js/global.js
+++ static/js/global.js
@@ -1,81 +1,99 @@
-$(document).ready(function() {
+;(function($, window) {
 
-	var current_file_id = 0;
-	var prev_count_files = 0;
-	var all_files = [];
-	var locked = false;
-  	var waiting = 0;
+	"use strict";
 
-	function noopHandler(evt) {
-		evt.stopPropagation();
-		evt.preventDefault();
-	}
+	var document = window.document;
 
-	function drop(evt) {
+	$(document).ready(function() {
 
-		noopHandler(evt);
+		var all_files = [],
+			current_file_id = 0,
+			locked = false,
+			prev_count_files = 0,
+			waiting = 0,
+			drop, dropzone, handleNextFile, handleReaderLoad, noopHandler;
 
-		var files = evt.dataTransfer.files;
-		var count = files.length;
+		noopHandler = function(evt) {
+			evt.stopPropagation();
+			evt.preventDefault();
+		};
 
-		if ( count > 0 ) {
+		drop = function(evt) {
 
-			prev_count_files = all_files.length;
+			noopHandler(evt);
 
-			if ( $("#dropzoneLabel").length != 0 )
-				$("#dropzone").html('');
+			var files = evt.dataTransfer.files,
+				count = files.length,
+				i, j;
 
-			for ( var i = prev_count_files + waiting, j = 0; i < prev_count_files + files.length + waiting; i++, j++ ) {
-				$("#dropzone").append('<div class="file ' + i + '"><div class="name">' + files[j].name + '</div><div class="progress">Waiting...</div></div>');
+			if ( count > 0 ) {
+
+				prev_count_files = all_files.length;
+
+				if ( $("#dropzoneLabel").length !== 0 ) {
+					$("#dropzone").html('');
+				}
+
+				for ( i = prev_count_files + waiting, j = 0; i < prev_count_files + files.length + waiting; i++, j++ ) {
+					$("#dropzone").append('<div class="file ' + i + '"><div class="name">' + files[j].name + '</div><div class="progress">Waiting...</div></div>');
+				}
+
+				waiting += count;
+
+				if ( ! locked ) {
+					waiting -= count;
+					all_files.push.apply(all_files, files);
+					handleNextFile();
+				}
 			}
+		};
 
-          	waiting += count;
+		handleReaderLoad = function(evt) {
 
-			if ( ! locked ) {
-              	waiting -= count;
-				all_files.push.apply(all_files, files);
+			var current_file = {};
+
+			current_file.name = all_files[current_file_id].name;
+			current_file.type = all_files[current_file_id].type;
+			current_file.contents = evt.target.result;
+
+			$.post('/upload', JSON.stringify(current_file), function(data, textStatus, jqXHR) {
+
+				if ( jqXHR.status == 200 ) {
+					$(".file." + current_file_id + " .progress").html("Uploaded");
+				} else {
+					$(".file." + current_file_id + " .progress").html("Failed");
+				}
+
+				all_files[current_file_id] = 1;
+				current_file_id++;
 				handleNextFile();
+			});
+		};
+
+		handleNextFile = function() {
+
+			if ( current_file_id < all_files.length ) {
+
+				locked = true;
+
+				$(".file." + current_file_id + " .progress").html("Uploading...");
+
+				var current_file = all_files[current_file_id],
+					reader = new FileReader();
+
+				reader.onload = handleReaderLoad;
+				reader.readAsDataURL(current_file);
+
+			} else {
+				locked = false;
 			}
-		}
-	}
+		};
 
-	function handleReaderLoad(evt) {
+		dropzone = document.getElementById("dropzone");
+		dropzone.addEventListener("dragenter", noopHandler, false);
+		dropzone.addEventListener("dragexit", noopHandler, false);
+		dropzone.addEventListener("dragover", noopHandler, false);
+		dropzone.addEventListener("drop", drop, false);
+	});
 
-		var current_file = {};
-
-		current_file.name = all_files[current_file_id].name;
-		current_file.type = all_files[current_file_id].type;
-		current_file.contents = evt.target.result;
-
-		$.post('/upload', JSON.stringify(current_file), function(data) {
-			all_files[current_file_id] = 1;
-			$(".file." + current_file_id + " .progress").html("Uploaded");
-			current_file_id++;
-			handleNextFile();
-		});
-	}
-
-	function handleNextFile() {
-
-		if ( current_file_id < all_files.length ) {
-
-			locked = true;
-
-			var current_file = all_files[current_file_id];
-
-			$(".file." + current_file_id + " .progress").html("Processing...");
-
-			var reader = new FileReader();
-			reader.onload = handleReaderLoad;
-			reader.readAsDataURL(current_file);
-		} else {
-			locked = false;
-		}
-	}
-
-	var dropzone = document.getElementById("dropzone");
-	dropzone.addEventListener("dragenter", noopHandler, false);
-	dropzone.addEventListener("dragexit", noopHandler, false);
-	dropzone.addEventListener("dragover", noopHandler, false);
-	dropzone.addEventListener("drop", drop, false);
-});
+}(jQuery, window));
